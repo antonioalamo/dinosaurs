3b9067db367709cc8a9eb445e067b415686cf228
antonioalamo <antonio.alamo_cl@yahoo.com>
2024-12-14 18:48:46 -0300

    images

--- src/data/dinosaurs.js
+++ src/data/dinosaurs.js
@@ -58,5 +58,105 @@ export const dinosaurs = [
     weight: "1 kg",
     image: "https://antonioalamo.github.io/dinosaurs/images/Pterodactilo.jpg",
     description: "El Pterodáctilo fue uno de los primeros reptiles voladores. Con sus alas membranosas y cuerpo ligero, dominaba los cielos del período Jurásico."
+  },
+  {
+    id: 7,
+    name: "Spinosaurus",
+    period: "Cretácico",
+    diet: "Carnívoro",
+    length: "15 metros",
+    weight: "7.5 toneladas",
+    image: "https://antonioalamo.github.io/dinosaurs/images/Spinosaurus.png",
+    description: "El Spinosaurus fue el dinosaurio carnívoro más grande conocido, con una distintiva vela en su espalda. Era un excelente nadador y cazador de peces."
+  },
+  {
+    id: 8,
+    name: "Ankylosaurus",
+    period: "Cretácico",
+    diet: "Herbívoro",
+    length: "8 metros",
+    weight: "6 toneladas",
+    image: "https://antonioalamo.github.io/dinosaurs/images/Ankylosaurus.jpg",
+    description: "El Ankylosaurus estaba cubierto de placas óseas y tenía una cola con un mazo que usaba como arma defensiva contra los depredadores."
+  },
+  {
+    id: 9,
+    name: "Brachiosaurus",
+    period: "Jurásico",
+    diet: "Herbívoro",
+    length: "25 metros",
+    weight: "35 toneladas",
+    image: "https://antonioalamo.github.io/dinosaurs/images/Brachiosaurus.jpg",
+    description: "El Brachiosaurus era uno de los dinosaurios más altos, con un cuello extremadamente largo que le permitía alcanzar la vegetación más alta."
+  },
+  {
+    id: 10,
+    name: "Parasaurolophus",
+    period: "Cretácico",
+    diet: "Herbívoro",
+    length: "10 metros",
+    weight: "3 toneladas",
+    image: "https://antonioalamo.github.io/dinosaurs/images/Parasaurolophus.jpg",
+    description: "El Parasaurolophus tenía una cresta tubular distintiva que usaba para comunicarse con otros de su especie mediante sonidos."
+  },
+  {
+    id: 11,
+    name: "Allosaurus",
+    period: "Jurásico",
+    diet: "Carnívoro",
+    length: "12 metros",
+    weight: "2 toneladas",
+    image: "https://antonioalamo.github.io/dinosaurs/images/Allosaurus.jpg",
+    description: "El Allosaurus fue uno de los principales depredadores del Jurásico, con garras afiladas y mandíbulas poderosas."
+  },
+  {
+    id: 12,
+    name: "Carnotaurus",
+    period: "Cretácico",
+    diet: "Carnívoro",
+    length: "7.5 metros",
+    weight: "1.5 toneladas",
+    image: "https://antonioalamo.github.io/dinosaurs/images/Carnotaurus.jpg",
+    description: "El Carnotaurus se caracterizaba por sus cuernos sobre los ojos y sus extremadamente pequeños brazos delanteros."
+  },
+  {
+    id: 13,
+    name: "Pachycephalosaurus",
+    period: "Cretácico",
+    diet: "Herbívoro",
+    length: "4.5 metros",
+    weight: "450 kg",
+    image: "https://antonioalamo.github.io/dinosaurs/images/Pachycephalosaurus.jpg",
+    description: "El Pachycephalosaurus tenía un grueso cráneo abovedado que probablemente usaba en combates con otros de su especie."
+  },
+  {
+    id: 14,
+    name: "Therizinosaurus",
+    period: "Cretácico",
+    diet: "Herbívoro",
+    length: "10 metros",
+    weight: "5 toneladas",
+    image: "https://antonioalamo.github.io/dinosaurs/images/Therizinosaurus.jpg",
+    description: "El Therizinosaurus poseía las garras más largas de cualquier animal terrestre conocido, que podían alcanzar hasta un metro de longitud."
+  },
+  {
+    id: 15,
+    name: "Giganotosaurus",
+    period: "Cretácico",
+    diet: "Carnívoro",
+    length: "13 metros",
+    weight: "8 toneladas",
+    image: "https://antonioalamo.github.io/dinosaurs/images/Giganotosaurus.jpg",
+    description: "El Giganotosaurus fue uno de los carnívoros más grandes, incluso mayor que el T-Rex, y dominaba los ecosistemas de Sudamérica."
+  },
+  {
+    id: 16,
+    name: "Archaeopteryx",
+    period: "Jurásico",
+    diet: "Carnívoro",
+    length: "0.5 metros",
+    weight: "1 kg",
+    image: "https://antonioalamo.github.io/dinosaurs/images/Archaeopteryx.jpg",
+    description: "El Archaeopteryx es considerado un eslabón evolutivo entre los dinosaurios y las aves modernas, con características de ambos grupos."
   }
 ]; 
