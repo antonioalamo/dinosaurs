64e51c6e91d88b932632ae95ee390cfc55883974
antonioalamo <antonio.alamo_cl@yahoo.com>
2024-12-14 18:20:05 -0300

    Inicial commit de la aplicación de dinosaurios

---
+++ src/data/dinosaurs.js
@@ -0,0 +1,62 @@
+export const dinosaurs = [
+  {
+    id: 1,
+    name: "Tyrannosaurus Rex",
+    period: "Cretácico",
+    diet: "Carnívoro",
+    length: "12 metros",
+    weight: "7 toneladas",
+    image: "https://upload.wikimedia.org/wikipedia/commons/9/94/Tyrannosaurus_Rex_Holotype.jpg",
+    description: "El Tyrannosaurus Rex fue uno de los depredadores más temibles que jamás haya existido. Con sus poderosas mandíbulas y dientes afilados, dominó los ecosistemas del Cretácico."
+  },
+  {
+    id: 2,
+    name: "Velociraptor",
+    period: "Cretácico",
+    diet: "Carnívoro",
+    length: "2 metros",
+    weight: "15 kg",
+    image: "images/Velociraptor.png",
+    description: "El Velociraptor era un dinosaurio ágil y veloz que cazaba en manada. A pesar de su tamaño relativamente pequeño, era un depredador muy efectivo."
+  },
+  {
+    id: 3,
+    name: "Diplodocus",
+    period: "Jurásico",
+    diet: "Herbívoro",
+    length: "27 metros",
+    weight: "25 toneladas",
+    image: "images/diplodocus.png",
+    description: "El Diplodocus fue uno de los dinosaurios más largos que existieron. Con su largo cuello podía alcanzar las hojas más altas de los árboles."
+  },
+  {
+    id: 4,
+    name: "Stegosaurus",
+    period: "Jurásico",
+    diet: "Herbívoro",
+    length: "9 metros",
+    weight: "5 toneladas",
+    image: "images/Stegosaurus.jpg",
+    description: "El Stegosaurus es famoso por las placas óseas a lo largo de su espalda y los picos en su cola. Estas características le servían tanto para regular su temperatura como para defenderse."
+  },
+  {
+    id: 5,
+    name: "Triceratops",
+    period: "Cretácico",
+    diet: "Herbívoro",
+    length: "8 metros",
+    weight: "12 toneladas",
+    image: "images/Triceratops.jpg",
+    description: "El Triceratops tenía tres cuernos distintivos y un gran collar óseo que usaba para defenderse de depredadores y competir con otros de su especie."
+  },
+  {
+    id: 6,
+    name: "Pterodáctilo",
+    period: "Jurásico",
+    diet: "Carnívoro",
+    length: "1 metro",
+    weight: "1 kg",
+    image: "images/Pterodactilo.jpg",
+    description: "El Pterodáctilo fue uno de los primeros reptiles voladores. Con sus alas membranosas y cuerpo ligero, dominaba los cielos del período Jurásico."
+  }
+]; 
